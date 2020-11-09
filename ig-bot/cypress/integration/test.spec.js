634285fcf6eb68c4c8363d686dfd9e8a93a40306
Josh W <jtwong10@gmail.com>
2020-11-08 23:17:44 -0800

    formatting

--- ig-bot/cypress/integration/test.spec.js
+++ ig-bot/cypress/integration/test.spec.js
@@ -1,24 +1,24 @@
 /// <reference types="cypress" />
 
 context('Window', () => {
-  const waitTime = Math.floor((Math.random() * 10) + 1000);
+    const waitTime = Math.floor((Math.random() * 10) + 1000);
 
-  before(() => {
-    cy.clearCookies()
-    cy.visit('https://instagram.com')
-  })
+    before(() => {
+        cy.clearCookies()
+        cy.visit('https://instagram.com')
+    })
 
-  it('get login field', () => {
-    cy.get('[name="username"]').type('')
-    cy.get('[name="password"]').type('')
-    cy.get('.sqdOP > .Igw0E').click({ force: true })
-    cy.wait(waitTime)
+    it('get login field', () => {
+        cy.get('[name="username"]').type('')
+        cy.get('[name="password"]').type('')
+        cy.get('.sqdOP > .Igw0E').click({ force: true })
+        cy.wait(waitTime)
 
-    // clear save login info message
-    cy.get('button').should('contain.text', 'Not Now').last().click()
-    cy.wait(waitTime)
+        // clear save login info message
+        cy.get('button').should('contain.text', 'Not Now').last().click()
+        cy.wait(waitTime)
 
-    // clear turn on notifications message
-    cy.get('button').should('contain.text', 'Not Now').last().click()
-  })
+        // clear turn on notifications message
+        cy.get('button').should('contain.text', 'Not Now').last().click()
+    })
 })
